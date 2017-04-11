22c110248b6fcc1c88bdfb0f04a6bbcfb9a28f44
konstantin <peredaj@bk.ru>
2017-04-11 17:28:28 +0700

    fix version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ksrv:autoform-fileuploader',
-  version: '0.0.6',
+  version: '0.0.7',
   summary: 'Yet another filuploader for autoform',
   git: 'https://github.com/ksrv/autoform-fileuploader',
   documentation: 'README.md'
